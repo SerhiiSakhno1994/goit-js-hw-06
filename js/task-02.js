2e884b8338470b422c1a77c586d165eb572a1c7b
Serhii <89796587+SerhiiSakhno1994@users.noreply.github.com>
2022-01-15 13:44:38 +0200

    update-task-02

--- js/task-02.js
+++ js/task-02.js
@@ -8,3 +8,17 @@ const ingredients = [
 ];
 
 const addList = document.querySelector('ul');
+
+const makeList = array => {
+  return ingredients.map(array => {
+    const elLi = document.createElement('li');
+    elLi.classList.add('item');
+    elLi.textContent = array;
+    return elLi;
+  });
+};
+
+const list = makeList(ingredients);
+console.log(list)
+
+addList.append(...list)
