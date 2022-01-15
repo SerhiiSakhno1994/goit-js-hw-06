886189bc2883b977c0461ab12a74f6e2fdee09f5
Serhii <89796587+SerhiiSakhno1994@users.noreply.github.com>
2022-01-15 19:24:08 +0200

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  // console.dir(formElements);
+
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+  
+  if (password == false || mail == false) {
+    alert("Заполните все поля");
+    return;
+  };
+
+  const formData = {
+    mail,
+    password,
+  };
+  console.log(formData);
+  form.reset();
+  
+};
